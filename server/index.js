0be0caa14fa2679743fae3d442a20927e86991d5
Kendal Enz <kendal.enz@gmail.com>
2023-12-13 19:42:46 -0500

    set up proper file configuration

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const app = require('./app');
+
+const setup = async()=> {
+  try {
+    console.log('starting');
+    const port = process.env.PORT || 3000;
+    app.listen(port, ()=> console.log(`listening on port ${port}`))
+  }
+  catch(err){
+    console.log(err)
+  }
+};
+    
+setup();
