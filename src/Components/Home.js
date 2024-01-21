ddfd65789a093f77d1407b65eceabc157d3fe393
Kendal Enz <kendal.enz@gmail.com>
2024-01-21 16:58:54 -0500

    created circle for mouse

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { range } from 'd3';
 
+const width = 960;
+const height = 500;
+const circleX = width / 2;
+const cicleY = height / 2;
+const circleRadius = 30;
 
 const Home = () => (
-   <div></div>
+  <div>
+    <svg width={width} height={height}>
+      <circle
+        cx={circleX}
+        cy={cicleY}
+        r={circleRadius}
+      >
+      </circle>
+    </svg>
+  </div>
 );
 
 export default Home;
